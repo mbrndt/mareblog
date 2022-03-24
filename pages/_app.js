c38e28fd7141100821252a58d99101134aa458d0
Mareike <mareike.brandt@hotmail.de>
2022-03-24 18:23:14 +0900

    fix: header, throwing error about children

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.scss'
 import { Layout } from '../components'
 
