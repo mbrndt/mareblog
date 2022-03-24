484a03cb330bb601faba16fb78525ecdf61d57f0
Mareike <mareike.brandt@hotmail.de>
2022-03-24 22:52:53 +0900

    feat: added display posts

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,25 +2,26 @@ import React, { useContext } from 'react'
 import Link from 'next/link'
 
 const categories = [
-  { name: 'React', slug: 'react' },
-  { name: 'Web Development', slug: 'web-dev' },
+  { name: 'live', slug: 'react' },
+  { name: 'laugh', slug: 'web-dev' },
+  { name: 'love', slug: 'cat-love' },
 ]
 
 const Header = () => {
   return (
     <div className="container mx-auto mb-8 px-10">
-      <div className="inline-block w-full border-b border-blue-400 py-8">
+      <div className="inline-block w-full border-b border-black py-8">
         <div className="block md:float-left">
           <Link href="/">
-            <span className="cursor-pointer text-4xl font-bold text-white">
-              mareblog
+            <span className="cursor-pointer text-4xl font-bold text-black">
+              choonsik
             </span>
           </Link>
         </div>
-        <div className="hidden md:float-left md:contents">
+        <div className="hidden text-black md:float-left md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="ml-4 mt-2 cursor-pointer align-middle font-semibold text-white md:float-right">
+              <span className="ml-4 mt-2 cursor-pointer align-middle font-semibold text-black md:float-right">
                 {category.name}
               </span>
             </Link>
--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,19 @@ export default function Home({ posts }) {
   return (
     <div className="container mx-auto  mb-8 px-10">
       <Head>
-        <title>mareblog</title>
+        <title>choonsik</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post) => (
             <div>
-              <PostCard post={post} key="{post.title}" />
+              <PostCard post={post.node} key="{post.title}" />
             </div>
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
-          <div className="relative top-8 lg:sticky">
+          <div className="relative top-8 text-black lg:sticky">
             <PostWidget />
             <Categories />
           </div>
