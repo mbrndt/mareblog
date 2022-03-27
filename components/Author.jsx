eaadb6bdef2fc936f1f2db4e465f08a14efe0e33
Mareike <mareike.brandt@hotmail.de>
2022-03-27 17:25:21 +0900

    feat: add author details

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Author = () => {
-  return <div>Author</div>
+const Author = ({ author }) => {
+  return (
+    <div className="relative mt-20 mb-8 rounded-lg bg-orange-900 bg-opacity-20 p-12 text-center">
+      <div className="absolute left-0 right-0 -top-14">
+        {' '}
+        <img
+          alt={author.height}
+          height="100px"
+          width="100px"
+          className="rounded-full align-middle"
+          src={author.photo.url}
+        />
+      </div>
+      <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+      <p className="text-lg normal-case">{author.bio}</p>
+    </div>
+  )
 }
 
 export default Author
