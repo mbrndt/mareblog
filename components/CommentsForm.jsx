833d99e7acbb83234f600fccfb4154bdbb9ba6b9
Mareike <mareike.brandt@hotmail.de>
2022-04-01 21:28:01 +0900

    debug: save comment

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,20 +1,27 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+
+import { submitComment } from '../services'
 
 const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false)
   const [localStorage, setLocalStorage] = useState(null)
   const [showSuccessMessage, setShowSuccessMessage] = useState(false)
-  const commentEl = useRef()
+  const commentEL = useRef()
   const nameEL = useRef()
   const emailEL = useRef()
   const storeDataEL = useRef()
 
+  useEffect(() => {
+    nameEL.current.value = window.localStorage.getItem('name')
+    emailEL.current.value = window.localStorage.getItem('email')
+  }, [])
+
   const handleCommentSubmission = () => {
     setError(false)
 
-    const { value: comment } = commentEl.current
-    const { value: name } = nameEl.current
-    const { value: email } = emailEl.current
+    const { value: comment } = commentEL.current
+    const { value: name } = nameEL.current
+    const { value: email } = emailEL.current
     const { checked: storeData } = storeDataEL.current
 
     if (!comment || !name || !email) {
@@ -30,25 +37,34 @@ const CommentsForm = ({ slug }) => {
     }
 
     if (storeData) {
-      localStorage.setItem('name', name)
-      localStorage.setItem('email', email)
+      window.localStorage.setItem('name', name)
+      window.localStorage.setItem('email', email)
     } else {
-      localStorage.removeItem('name', name)
-      localStorage.removeItem('email', email)
+      window.localStorage.removeItem('name', name)
+      window.localStorage.removeItem('email', email)
     }
+
+    submitComment(commentObj).then((res) => {
+      setShowSuccessMessage(true)
+      setTimeout(() => {
+        setShowSuccessMessage(false)
+      }, 3000)
+    })
   }
 
   return (
     <div className="ob-12 mb-8 rounded-lg bg-white p-8 shadow-lg">
-      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">CommentsForm</h3>
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+        Leave A Reply
+      </h3>
 
       <div className="mb-4 grid grid-cols-1 gap-4">
         Comment
         <textarea
-          ref={commentEl}
+          ref={commentEL}
           placeholder="enter comment here ..."
           name="comment"
-          className="w-full rounded-lg bg-gray-200 p-4 text-gray-200 outline-none focus:ring-2 focus:ring-gray-200"
+          className="w-full rounded-lg bg-gray-200 p-4 text-gray-500 outline-none focus:ring-2 focus:ring-gray-200"
         />
       </div>
       <h1 className="p-2">Name and Email</h1>
@@ -58,14 +74,14 @@ const CommentsForm = ({ slug }) => {
           ref={nameEL}
           placeholder="enter name here "
           name="name"
-          className="w-full rounded-lg bg-gray-200 px-4 py-2 text-gray-200 outline-none focus:ring-2 focus:ring-gray-200"
+          className="w-full rounded-lg bg-gray-200 px-4 py-2 text-gray-500 outline-none focus:ring-2 focus:ring-gray-200"
         />
         <input
           type="text"
           ref={emailEL}
           placeholder="enter e-mail address here "
           name="email"
-          className="w-full rounded-lg bg-gray-200 px-4 py-2 text-gray-200 outline-none focus:ring-2 focus:ring-gray-200"
+          className="w-full rounded-lg bg-gray-200 px-4 py-2 text-gray-500 outline-none focus:ring-2 focus:ring-gray-200"
         />
       </div>
       <div className="mb-4 grid grid-cols-1 gap-4">
--- pages/api/comments.js
+++ pages/api/comments.js
@@ -2,7 +2,14 @@ import { GraphQLClient, gql } from 'graphql-request'
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
-export default function comments(req, res) {
+/** *************************************************************
+ * Any file inside the folder pages/api is mapped to /api/* and  *
+ * will be treated as an API endpoint instead of a page.         *
+ *************************************************************** */
+
+// export a default function for API route to work
+export default async function asynchandler(req, res) {
+  const { name, email, slug, comment } = req.body
   const graphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
       authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
@@ -10,8 +17,33 @@ export default function comments(req, res) {
   })
 
   const query = gql`
-mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!{
-createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) {id} {
-} 
+    mutation CreateComment(
+      $name: String!
+      $email: String!
+      $comment: String!
+      $slug: String!
+    ) {
+      createComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
+        id
+      }
+    }
   `
+
+  try {
+    const result = await graphQLClient.request(query, req.body)
+    return res.status(200).send(result)
+  } catch (error) {
+    console.log(error)
+  }
+
+  const result = await graphQLClient.request(query, req.body)
+
+  return res.status(200).send(result)
 }
